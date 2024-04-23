71b3ffc541fedb134ee6c1e3ec5eafaa6a621813
Sam Uherek <samuherekbiz@gmail.com>
2024-04-23 18:47:44 +0200

    added factors

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ use std::fmt::Debug;
 
 struct Res<T: Debug> {
     value: T,
+    ticks: usize,
 }
 
 impl<T: Debug> Res<T> {
-    fn new(value: T) -> Self {
-        Self { value }
+    fn new(value: T, ticks: usize) -> Self {
+        Self { value, ticks }
     }
 
     fn print(&self) {
         println!("result: {:?}", self.value);
+        println!("ticks: {}", self.ticks);
     }
 }
 
@@ -21,31 +23,33 @@ fn median(input: &[usize]) -> Res<f64> {
 
     if len % 2 != 0 {
         let result = values[(len - 1) / 2] as f64;
-        Res::new(result)
+        Res::new(result, 1)
     } else {
         let result = (values[len / 2] + values[(len / 2) - 1]) as f64 / 2.0;
-        Res::new(result)
+        Res::new(result, 1)
     }
 }
 
 fn largest(input: &[usize], count: usize) -> Res<Vec<usize>> {
     let mut values = input.to_vec();
     values.sort();
-    Res::new(values[(values.len() - count)..].to_vec())
+    Res::new(values[(values.len() - count)..].to_vec(), 1)
 }
 
 fn rotate_left(input: &[usize], offset: usize) -> Res<Vec<usize>> {
     let len = input.len();
     let mut res = vec![0; len];
     let offset = offset % len;
+    let mut ticks = 0;
     for (i, v) in input.iter().enumerate() {
+        ticks += 1;
         if i < offset {
             res[len - offset + i] = *v;
         } else {
             res[i - offset] = *v;
         }
     }
-    Res::new(res)
+    Res::new(res, ticks)
 }
 
 struct ListNode<T> {
@@ -98,6 +102,23 @@ fn list(input: &[usize]) -> LinkedList<usize> {
     list
 }
 
+fn factors(input: i32) -> Res<Vec<i32>> {
+    let mut result = vec![];
+    let end = f64::sqrt(input.into()).round() as i32;
+    let mut ticks = 0;
+    for i in 1..=end {
+        ticks += 1;
+        if input % i == 0 {
+            result.push(i);
+            if i != end {
+                result.push(input / i);
+            }
+        }
+    }
+    result.sort();
+    Res::new(result, ticks)
+}
+
 fn start(name: &str, input: &[usize]) {
     println!("");
     println!("----------- {name}");
@@ -118,4 +139,8 @@ fn main() {
 
     start(&"LIST", &input);
     println!("{:?}", list(&input));
+
+    let value = 2398;
+    start(&"FACTOR", &[value]);
+    factors(value as i32).print();
 }
